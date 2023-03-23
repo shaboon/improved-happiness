f20d22bfebe5d1bc4057f6130ae3a887259c737d
Tyrrance <tyrrancem37@gmail.com>
2023-03-23 14:47:07 -0400

    Set up weather api data and weather cards.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
+var apiKey = '0578539c9ba7ff6fc8e3f6f54cac232c';
 var reserveBtn = $("#reserve");
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 reserveBtn.on("click", function () {
@@ -17,9 +18,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -35,3 +36,42 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+function getCurrentWeather(lat, lon) {
+  fetch('https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // CURRENT WEATHER DATA
+      console.log(data);
+    });
+}
+
+function getFiveDayWeather(lat, lon) {
+  fetch('https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // 5 DAY WEATHER DATA
+      console.log(data);
+    });
+}
+
+function init() {
+  var city = 'Mekoryuk';
+
+  fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=' + 1 + '&appid=' + apiKey)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      getCurrentWeather(lat, lon);
+      getFiveDayWeather(lat, lon);
+    });
+}
+
+init();
