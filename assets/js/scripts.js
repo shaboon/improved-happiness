6dcce8cb090bbf3f9ec14b0cad047d39e4147927
Win.TowerSH <116582145+shaboon@users.noreply.github.com>
2023-03-23 19:22:27 -0400

    carousel is working, API left

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-var apiKey = '0578539c9ba7ff6fc8e3f6f54cac232c';
+var apiKey = "0578539c9ba7ff6fc8e3f6f54cac232c";
 var reserveBtn = $("#reserve");
 var slidePosition = 1;
 SlideShow(slidePosition);
@@ -7,45 +7,32 @@ reserveBtn.on("click", function () {
   window.location.href = "./reserve.html";
 });
 
-// forward/Back controls
-function plusSlide(n) {
-  SlideShow(slidePosition += n);
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
 
-//  images controls
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
-}
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
 
-function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var circles = document.getElementsByClassName("dot");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace("enable", "");
-  }
-  slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
-} 
-
-function getCurrentWeather(lat, lon) {
-  fetch('https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      // CURRENT WEATHER DATA
-      console.log(data);
-    });
-}
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
 
 function getFiveDayWeather(lat, lon) {
-  fetch('https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      apiKey
+  )
     .then(function (response) {
       return response.json();
     })
@@ -56,16 +43,22 @@ function getFiveDayWeather(lat, lon) {
 }
 
 function init() {
-  var city = 'Mekoryuk';
+  var city = "Mekoryuk";
 
-  fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=' + 1 + '&appid=' + apiKey)
+  fetch(
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+      city +
+      "&limit=" +
+      1 +
+      "&appid=" +
+      apiKey
+  )
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       var lat = data[0].lat;
       var lon = data[0].lon;
-      getCurrentWeather(lat, lon);
       getFiveDayWeather(lat, lon);
     });
 }
